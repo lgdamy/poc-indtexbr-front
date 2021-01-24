d674836acc3db8fcbee04dea34deb4996323367f
tcc_puc_ls <srochg@gmail.com>
2021-01-24 14:21:03 -0300

    mensagem 2

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entry.component.scss']
 })
 export class EntryComponent implements OnInit {
-  title="Bem-vindos Piroks"
+  title="Bem-vindos Piroks 1"
   message="Esta corporação não seria nada sem vocês"
   constructor() { }
 
