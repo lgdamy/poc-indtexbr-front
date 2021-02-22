589cb01e4a7f68a854263b32332c31261a5a9723
tcc_puc_ls <srochg@gmail.com>
2021-02-22 20:48:00 -0300

    Correção da rota de autenticação, pq meu design não presta atenção

--- src/environments/environment.jenkins.ts
+++ src/environments/environment.jenkins.ts
@@ -4,5 +4,6 @@ export const environment = {
   url_authentication:'http://192.168.99.100:31380/oauth',
   // url_processo_industrial:'http://localhost:8080/api',
   url_consultorias_assessorias:'https://poc-consultorias-assessorias.herokuapp.com/api',
-  url_normas:'https://poc-normas.herokuapp.com/api'
+  url_normas:'https://poc-normas.herokuapp.com/api',
+  api_auth_token:'Basic Y2xpZW50SWQ6c2VjcmV0'
 };
