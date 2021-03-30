c60c1986cb675c0a18c71abef1e91719fe96954f
tcc_puc_ls <srochg@gmail.com>
2021-03-29 21:20:52 -0300

    Alterando docker file

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,16 @@ RUN npm install
 RUN npm install -g @angular/cli@9.1.12
 COPY . .
 
-ARG PROFILE
+##ARG PROFILE
 ##ENV PROFILE jenkins
 
 RUN echo "Environment: ${PROFILE}"
 ##RUN npm run build-${PROFILE}
-RUN npm run build
+RUN npm run build --prod
 
 ##ARG NODE_ENV
 ##ENV NODE_ENV jenkins
 
 ##CMD npm run start-jenkins
-CMD npm run start
+CMD npm run start --prod
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  // url_processo_industrial:'https://poc-indtexbr-proc-industrial.herokuapp.com/api',
-  url_processo_industrial:'http://localhost:8089/processoindustrial', 
-  url_websocket:'http://localhost:8089/stomp',
-  url_consultorias_assessorias:'https://poc-consultorias-assessorias.herokuapp.com/api',
-  url_normas:'https://poc-normas.herokuapp.com/api',
-  url_authentication:'http://localhost:8089/oauth',
+  production: true,
+  url_processo_industrial:'http://api.tcc2021puc-luizsergio.cf/processoindustrial',
+  url_websocket: 'http://api.tcc2021puc-luizsergio.cf/stomp',
+  url_authentication:'http://api.tcc2021puc-luizsergio.cf/oauth',
   api_auth_token:'Basic Y2xpZW50SWQ6c2VjcmV0'
 };
 
